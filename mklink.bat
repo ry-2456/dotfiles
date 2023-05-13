621d0e4a7f4a8e906ddd1bbb33e457defbaa2d9b
rintaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2023-05-13 17:58:12 +0900

    mklink.bat実行時にVundle.vimがなければダウンロード

--- mklink.bat
+++ mklink.bat
@@ -2,3 +2,8 @@ rem make symbolic links for vim settings
 rem run "rm -rf symbliclink" when removing symbliclink (do not add "\" to the end of the folder)
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
 mklink /D %HOMEPATH%\vimfiles\colors %HOMEPATH%\dotfiles\colors
+
+rem install Vundle.vim if not installed
+if exist "%HOMEPATH%\bundle\Vundle.vim" (
+  git clone https://github.com/VundleVim/Vundle.vim %HOMEPATH%\bundle\Vundle.vim 
+)
