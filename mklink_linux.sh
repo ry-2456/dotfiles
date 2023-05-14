13b4a1095cf12458b0f8bdd37f91db6d2379959e
rintaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2023-05-14 16:32:23 +0900

    mklink.sh実行時にVundle.vimの最新版ダウンロード

--- mklink_linux.sh
+++ mklink_linux.sh
@@ -7,9 +7,10 @@ if [ ! -e ~/.vim ]; then
 fi
 
 # install Vundle.vim
-if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+if [ -e ~/.vim/bundle/Vundle.vim ]; then
+  rm -rf ~/.vim/bundle/Vundle.vim
 fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
