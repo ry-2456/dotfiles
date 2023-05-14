5c09a6118fefe39e45d2651b431d7d2c03610b0c
rintaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2023-05-14 20:38:13 +0900

    .vimsessions作成

--- setting_linux.sh
+++ setting_linux.sh
@@ -6,11 +6,16 @@ if [ ! -e ~/.vim ]; then
   mkdir ~/.vim
 fi
 
+# make .vimsessions dir to save vim sessions
+if [ ! -e ~/.vimsessions ]; then
+  mkdir ~/.vimsessions
+fi
+
+ln -sf ~/dotfiles/colors ~/.vim/colors
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
 # install Vundle.vim
 if [ -e ~/.vim/bundle/Vundle.vim ]; then
   rm -rf ~/.vim/bundle/Vundle.vim
 fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-ln -sf ~/dotfiles/colors ~/.vim/colors
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
