a5789c2b6ad73169b19db3c3ef2f86de23295b1d
rintaro-takashiro <ittanmomennimakaretyaimashita@gmail.com>
2023-05-14 09:34:44 +0900

    Vundle.vimの最新版ダウンロードするように変更

--- mklink.bat
+++ mklink.bat
@@ -6,6 +6,8 @@ mklink /D %HOMEPATH%\vimfiles\colors %HOMEPATH%\dotfiles\colors
 @rem install Vundle.vim if not installed
 @rem set VUNDLE_INSTALL_PATH = %HOMEPATH%\vimfiles\bundle\Vundle.vim
 @rem echo %VUNDLE_INSTALL_PATH%
-if not exist %HOMEPATH%\vimfiles\bundle\Vundle.vim (
-  git clone https://github.com/VundleVim/Vundle.vim.git %HOMEPATH%\vimfiles\bundle\Vundle.vim 
+if exist %HOMEPATH%\vimfiles\bundle\Vundle.vim (
+  rmdir /s /q %HOMEPATH%\vimfiles\bundle\Vundle.vim
 )
+git clone https://github.com/VundleVim/Vundle.vim.git %HOMEPATH%\vimfiles\bundle\Vundle.vim 
+
