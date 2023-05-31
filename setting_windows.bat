4cc9d19766f895284e960ba1018268d0b97f4a82
rintaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2023-05-31 21:51:36 +0900

    setting_windows.batで.vimrc->vimrcに更新

--- setting_windows.bat
+++ setting_windows.bat
@@ -1,6 +1,6 @@
 @rem make symbolic links for vim settings
 @rem run "rm -rf symbliclink" when removing symbliclink (do not add "\" to the end of the folder)
-mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
+mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\vimrc
 mklink /D %HOMEPATH%\vimfiles\colors %HOMEPATH%\dotfiles\colors
 
 @rem make .vimsessions dir to save vim sessions
